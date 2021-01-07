7ce74f395bff8c97cf14673d6f6bd4c5a6ea8b77
Niclasapdk <74359616+Niclasapdk@users.noreply.github.com>
2021-01-07 14:33:29 +0100

    logik update

--- Logik.py
+++ Logik.py
@@ -1,14 +1,20 @@
+import Data
 
-class MastermindLogik:
+class MastermindLogik(Data.MastermindData):
 
     def __init__(self):
-        pass
-
-    Farvemap = {"Rød":1, "Lilla":2, "Blå":3, "Gul":4, "Grøn":5, "Orange":6, "Hvid":7, "Pink":8}
+        self.Farvemap = {"Rød":1, "Lilla":2, "Blå":3, "Gul":4, "Grøn":5, "Orange":6, "Hvid":7, "Pink":8}
 
     def konverter(self, farve1, farve2, farve3, farve4):
-       return [Farvemap[farve1], Farvemap[farve2], Farvemap[farve3], Farvemap[farve4]]
+       return [self.Farvemap[farve1], self.Farvemap[farve2], self.Farvemap[farve3], self.Farvemap[farve4]]
 
+
+    def match(self, code, guess):
+
+       codeconverted = self.konverter(code[0], code[1], code[2], code[3])
+       guessconverted = self.konverter(guess[0], guess[1], guess[2], guess[3])
+
+       return self.check(codeconverted, guessconverted)
 
 
 
