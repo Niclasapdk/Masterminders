5a9bfe2e48cd498ea363d522e64bbb4a94b96724
ArmoredKiwi <74359644+ArmoredKiwi@users.noreply.github.com>
2021-01-07 14:14:04 +0100

    Data change

--- Data.py
+++ Data.py
@@ -2,13 +2,18 @@
 class MastermindData:
 
     def __init__(self):
-        pass
+        self.buffer = 'buffer'
 
+    def check(self, code, guess):
+        for i in range(4):
+            if code[i] == guess[i]:
+                guess[i] = 'check'
 
-    def 
+            if guess[i] == code[0] or guess[i] == code[1] or guess[i] == code[2] or guess[i] == code[3]:
+                guess[i] = 'half'
 
-
-
-
+            if guess[i] != 'check' and guess[i] != 'half':
+                guess[i] = 'wrong'
+        return guess
 
 
