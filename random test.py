d43351823a9a0f50c5a475201d8088d0f7b8c370
Vollenator <74359785+Vollenator@users.noreply.github.com>
2021-01-11 09:08:48 +0100

    Modified GUI.py

--- random test.py
+++ random test.py
@@ -1,17 +1,17 @@
-import tkinter as tk
-from tkinter import font as tkfont
+from tkinter import *
+from PIL import ImageTk, Image
+import webbrowser
 
 
-class SampleApp(tk.Tk):
+class SampleApp(Tk):
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+        Tk.__init__(self, *args, **kwargs)
 
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
-        container = tk.Frame(self)
+        container = Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -30,32 +30,61 @@ class SampleApp(tk.Tk):
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
+        #Show a frame for the given page name
         frame = self.frames[page_name]
         frame.tkraise()
 
+def Rules():
+    print("rules have been pressed")
+    webbrowser.open("Rules.txt")
 
-class StartPage(tk.Frame):
+class StartPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.controller.title("Main Menu")
+        self.controller.geometry("800x600")
+        self.configure(bg="royalblue2")
+        self.img = Image.open("mastermind.png")
+        self.imageSizeWidth, self.imageSizeHeight = self.img.size
+        self.n = 0.25
+        self.newImageSizeWidth = int(self.imageSizeWidth * self.n)
+        self.newImageSizeHeight = int(self.imageSizeHeight * self.n)
+        self.image = self.img.resize((self.newImageSizeWidth, self.newImageSizeHeight), Image.ANTIALIAS)
+
+        self.test = ImageTk.PhotoImage(self.image)
+
+        self.titleimg = Label(self, image=self.test).place(relx=0.5, rely=0.19, anchor=CENTER)
+        # self.Title = Label(self.root, text="MASTERMIND",font=("Times", "50", "bold"), bg="royalblue2", fg ="red4").place(relx=0.5, rely=0.2, anchor=CENTER)
+
+        self.StartButton = Button(self, text="Start Game", command=lambda: controller.show_frame("PageOne"), font=("Arial", "25"), bg="black",
+                                  fg="white").place(relx=0.5, rely=0.45, anchor=CENTER)
+        self.RuleButton = Button(self, text="Rules", command=Rules, font=("Arial", "25"), bg="black",
+                                 fg="white").place(relx=0.5, rely=0.6, anchor=CENTER)
+
+
+
+        #button1 = Button(self, text="Go to Page One",
+                            #command=lambda: controller.show_frame("PageOne"))
+        #button1.pack()
+
+
+
+
+class PageOne(Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+
+        self.controller.title("Game")
+        self.controller.geometry("800x600")
+        self.configure(bg="royalblue2")
+
+        label = Label(self, text="This is page 1")
         label.pack(side="top", fill="x", pady=10)
-
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"))
-        button1.pack()
-
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        button = Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
