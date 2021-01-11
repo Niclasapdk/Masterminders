9c48c59f00c13a1b3b7e871e2aaeff0876945199
Vollenator <74359785+Vollenator@users.noreply.github.com>
2021-01-11 09:10:34 +0100

    Modified GUI.py

--- GUI.py
+++ GUI.py
@@ -1,66 +1,92 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import webbrowser
 
 
-class mainmenu():
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Main Menu")
-        self.root.geometry("800x600")
-        self.root.configure(bg="royalblue2")
+class SampleApp(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, PageOne):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        # Show a frame for the given page name
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+def Rules():
+    print("rules have been pressed")
+    webbrowser.open("Rules.txt")
+
+
+class StartPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.controller.title("Main Menu")
+        self.controller.geometry("800x600")
+        self.configure(bg="royalblue2")
         self.img = Image.open("mastermind.png")
         self.imageSizeWidth, self.imageSizeHeight = self.img.size
-        self.n=0.25
+        self.n = 0.25
         self.newImageSizeWidth = int(self.imageSizeWidth * self.n)
         self.newImageSizeHeight = int(self.imageSizeHeight * self.n)
         self.image = self.img.resize((self.newImageSizeWidth, self.newImageSizeHeight), Image.ANTIALIAS)
 
         self.test = ImageTk.PhotoImage(self.image)
 
-        self.titleimg = Label(self.root, image=self.test).place(relx=0.5, rely=0.19, anchor=CENTER)
-        #self.Title = Label(self.root, text="MASTERMIND",font=("Times", "50", "bold"), bg="royalblue2", fg ="red4").place(relx=0.5, rely=0.2, anchor=CENTER)
+        self.titleimg = Label(self, image=self.test).place(relx=0.5, rely=0.19, anchor=CENTER)
+        # self.Title = Label(self.root, text="MASTERMIND",font=("Times", "50", "bold"), bg="royalblue2", fg ="red4").place(relx=0.5, rely=0.2, anchor=CENTER)
 
-        self.StartButton = Button(self.root, text="Start Game", command=self.start,font=("Arial", "25"), bg="black", fg ="white").place(relx=0.5, rely=0.45, anchor=CENTER)
-        self.RuleButton = Button(self.root, text="Rules", command=self.rules,font=("Arial", "25"), bg="black", fg ="white").place(relx=0.5, rely=0.6, anchor=CENTER)
+        self.StartButton = Button(self, text="Start Game", command=lambda: controller.show_frame("PageOne"),
+                                  font=("Arial", "25"), bg="black",
+                                  fg="white").place(relx=0.5, rely=0.45, anchor=CENTER)
+        self.RuleButton = Button(self, text="Rules", command=Rules, font=("Arial", "25"), bg="black",
+                                 fg="white").place(relx=0.5, rely=0.6, anchor=CENTER)
 
-    def start(self):
-        print("start has been pressed")
-        Game(Toplevel(self.root))
-
-    def rules(self):
-        print("rules have been pressed")
-        Rules(Toplevel(self.root))
-
-
-class Rules(mainmenu):
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Rules")
-        self.root.geometry("400x370")
-
-        self.configfile = Label(self.root, width=45, height=20)
-        self.configfile.place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.text = open("Rules.txt")
-        self.t = self.text.readlines()
-        self.configfile.config(text=self.t)
-        #de
+        # button1 = Button(self, text="Go to Page One",
+        # command=lambda: controller.show_frame("PageOne"))
+        # button1.pack()
 
 
+class PageOne(Frame):
 
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
 
-class Game(mainmenu):
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Game")
-        self.root.geometry("400x370")
+        self.controller.title("Game")
+        self.controller.geometry("800x600")
+        self.configure(bg="royalblue2")
 
-
-def main():
-    root = Tk()
-    app = mainmenu(root)
-    root.mainloop()
+        label = Label(self, text="This is page 1")
+        label.pack(side="top", fill="x", pady=10)
+        button = Button(self, text="Go to the start page",
+                        command=lambda: controller.show_frame("StartPage"))
+        button.pack()
 
 
 if __name__ == "__main__":
-    main()
-
+    app = SampleApp()
+    app.mainloop()
