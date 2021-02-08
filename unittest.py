925a7a5f43c5571842bdbc49fece5f211d032fdb
Niclasapdk <74359616+Niclasapdk@users.noreply.github.com>
2021-02-08 10:09:45 +0100

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -2,3 +2,20 @@ import Logik
 import unittest
 
 
+class TestafLogikogdatalag(unittest.TestCase):
+
+    def test1(self):
+        xd = Logik.MastermindLogik()
+        self.assertEqual(xd.talkonverter(1), "Rød")
+
+    def test2(self):
+        xd = Logik.MastermindLogik()
+        self.assertEqual(xd.konverter("Rød"), 1)
+
+    def test3(self):
+        
+
+
+
+if __name__ == "__main__":
+    unittest.main()
